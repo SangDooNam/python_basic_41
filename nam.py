e5b35ce99070cc056dca43714e5c0317b735de63
SangDoo Nam <kexe009@hotmail.com>
2023-09-28 21:02:04 +0200

    finishi nam.py

--- nam.py
+++ nam.py
@@ -1,15 +1,15 @@
 """Task 1"""
 
 
-settings = {'title': 'My original title'}
+# settings = {'title': 'My original title'}
 
 
 
-def change_site_title(string):
+# def change_site_title(string):
     
-    settings['title'] = string
+#     settings['title'] = string
     
-    return settings
+#     return settings
     
 
 # print(settings)
@@ -18,15 +18,86 @@ def change_site_title(string):
 
 """Task 2"""
 
-default_settings = {'title' : 'My original title'}
+# default_settings = {'title' : 'My original title'}
 
-def get_title(dict = default_settings):
+# def get_title(dict = default_settings):
     
-    return dict['title']
+#     return dict['title']
 
 
-print(get_title(settings))
-print(get_title())
-change_site_title("A new fancy title")
-print(get_title(settings))
-print(get_title())
+# print(get_title(settings))
+# print(get_title())
+# change_site_title("A new fancy title")
+# print(get_title(settings))
+# print(get_title())
+
+
+
+"""Task 3"""
+
+# settings = {'title': 'My original title', 'pages' : []}
+# default_settings = {'title' : 'My original title', 'pages': []}
+
+# def get_pages(settings = default_settings):
+    
+#     return settings['pages']
+    
+
+
+# def add_pages(page, settings = default_settings):
+    
+#     settings['pages'].append(page)
+
+    
+
+
+# home = {"title": "Home", "path": "/"}
+# add_pages(home)
+# print(get_pages())
+# print(get_pages(settings))
+# about = {"title": "About", "path": "/about/"}
+# add_pages(about, settings)
+# print(get_pages())
+# print(get_pages(settings))
+
+
+"""Task 4"""
+
+
+def print_user_profile(gender = 'female', 
+                       first = None, 
+                       last = 'Doe', 
+                       pictures = []):
+    if first is None:
+        if gender == 'male':
+            
+            first = 'John'
+        elif gender == 'female':
+            
+            first = 'Jane'
+    
+    print(f'The user {first} {last} has the following pictures:\ncommon_header.png')
+    
+    for i in pictures:
+        
+        print(i)
+    
+    
+
+test_data1 = {
+    "gender": "male",
+    "last": "Brown",
+    "pictures": ["holidays1.png", "easter_grandma.png"]
+}
+test_data2 = {
+    "first": "Alicia",
+    "last": "Schmidt"
+}
+test_data3 = {
+    "last": "Korkov",
+    "pictures": ["sunset.png"]
+}
+print_user_profile(**test_data1)
+print_user_profile(**test_data2)
+print_user_profile(**test_data3)
+print_user_profile(**test_data2)
