5e577b1e89d491968c51bbe48eeb403efe580d35
SangDoo Nam <kexe009@hotmail.com>
2023-09-29 11:45:27 +0200

    modify nam.py after solution

--- nam.py
+++ nam.py
@@ -64,19 +64,72 @@
 """Task 4"""
 
 
+# def print_user_profile(gender = 'female', 
+#                        first = None, 
+#                        last = 'Doe', 
+#                        pictures = []):
+#     if first is None:
+#         if gender == 'male':
+            
+#             first = 'John'
+#         elif gender == 'female':
+            
+#             first = 'Jane'
+    
+#     print(f'The user {first} {last} has the following pictures:\ncommon_header.png')
+    
+#     for i in pictures:
+        
+#         print(i)
+    
+    
+
+# test_data1 = {
+#     "gender": "male",
+#     "last": "Brown",
+#     "pictures": ["holidays1.png", "easter_grandma.png"]
+# }
+# test_data2 = {
+#     "first": "Alicia",
+#     "last": "Schmidt"
+# }
+# test_data3 = {
+#     "last": "Korkov",
+#     "pictures": ["sunset.png"]
+# }
+# print_user_profile(**test_data1)
+# print_user_profile(**test_data2)
+# print_user_profile(**test_data3)
+# print_user_profile(**test_data2)
+
+
+
+
 def print_user_profile(gender = 'female', 
                        first = None, 
                        last = 'Doe', 
                        pictures = []):
     if first is None:
-        if gender == 'male':
-            
-            first = 'John'
-        elif gender == 'female':
+        
+        if gender == 'female':
             
             first = 'Jane'
+            
+        elif gender == 'male':
+            
+            first = 'John'
     
-    print(f'The user {first} {last} has the following pictures:\ncommon_header.png')
+    print(f'The user {first} {last} has the following pictures:')
+    
+    
+    if len(pictures) != 0:
+        
+        if pictures[0] != 'common_header.png':
+            
+            pictures.insert(0, 'common_header.png')
+    else:
+        pictures.insert(0, 'common_header.png') 
+
     
     for i in pictures:
         
